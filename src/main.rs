570b2e7ff27007af60d44b43ae90658a24b2f04a
WinsomeQuill <artemqa.belyakov07@gmail.com>
2022-10-24 17:53:39 +0700

    Added methods for struct User

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,21 @@
 struct User<'a> {
-    name: &'a str
+    name: &'a str,
+    age: u16,
+}
+
+impl <'a>User<'a> {
+    fn new(name: &str, age: u16) -> Result<User, Box<dyn std::error::Error>> { // создаем структуру
+        let user = User { name, age };
+        Ok(user)
+    }
+
+    fn about(&self) { // выводим формат информацию о структуре
+        let formating = format!("My name: {}. Age: {}", &self.name, self.age);
+        println!("{}", formating);
+    }
 }
 
 fn main() {
-    let user = User { name: "Artem" };
+    let user = User::new("Artem", 19).unwrap();
+    user.about();
 }
